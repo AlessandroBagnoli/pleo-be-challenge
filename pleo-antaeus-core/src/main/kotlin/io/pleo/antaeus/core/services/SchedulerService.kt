459a9e44e1536b92341945faadad48406675c50e
Alessandro Bagnoli <a.bagnoli@alpiantech.com>
2023-03-09 13:59:47 +0100

    feat: first naive implementation of the scheduler using coroutine

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/SchedulerService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/SchedulerService.kt
@@ -1,15 +1,27 @@
 package io.pleo.antaeus.core.services
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import mu.KotlinLogging
 
 class SchedulerService(
-  billingService: BillingService
+  private val billingService: BillingService
 ) {
 
   private val log = KotlinLogging.logger {}
 
   fun start() {
     log.info { "Starting SchedulerService" }
+    CoroutineScope(Dispatchers.Default).launch {
+      while (true) {
+        delay(1000) // Wait until the first day of next month
+        // Code to run every first of the month goes here
+        log.info { "Running task on the first of the month" }
+        billingService.performBilling()
+      }
+    }
   }
 
 }
