3e74c48cac8d02aa17bbad00f68a4a757d37899e
Alessandro Bagnoli <a.bagnoli@alpiantech.com>
2023-03-09 12:11:00 +0100

    feat: added InvoiceService as dependency in BillingService

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -3,7 +3,8 @@ package io.pleo.antaeus.core.services
 import io.pleo.antaeus.core.external.PaymentProvider
 
 class BillingService(
-  private val paymentProvider: PaymentProvider
+  private val paymentProvider: PaymentProvider,
+  private val invoiceService: InvoiceService
 ) {
 // TODO - Add code e.g. here
 }
