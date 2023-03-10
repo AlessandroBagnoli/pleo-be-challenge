a6654905668e1a58374dc0380b87f0c753e4aee6
Alessandro Bagnoli <a.bagnoli@alpiantech.com>
2023-03-10 10:43:48 +0100

    chore: docker-compose, modified scripts

--- docker-start.sh
+++ docker-start.sh
@@ -3,13 +3,4 @@
 set -x
 
 # Create a new image version with latest code changes.
-docker build . --tag pleo-antaeus
-
-# Build the code.
-docker run \
-  --publish 8080:8080 \
-  --rm \
-  --interactive \
-  --tty \
-  --volume pleo-antaeus-build-cache:/root/.gradle \
-  pleo-antaeus
+docker compose --profile app up -d
